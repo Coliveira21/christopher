61c3cb669d4cd78470215418486ab29c54150bea
Coliveira21 <66383980+Coliveira21@users.noreply.github.com>
2020-06-04 09:01:31 +0100

    Create try.php

---
+++ try.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Project</title>
+</head>
+<body>
+	<?php
+		echo "hello world";  
+	?>
+	<form>
+		<p>name</p>
+		<input type="text" name="name">
+		<input type="submit" name="send">
+	</form>
+
+</body>
+</html>
